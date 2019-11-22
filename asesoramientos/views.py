2ab99bcca5cd68dbfa22fd1127153a0e94193dd6
Fernando Andres-Pretel <fandresp@sescam.jccm.es>
2019-11-22 09:10:13 +0100

    Instalación rpy2

--- asesoramientos/views.py
+++ asesoramientos/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import rpy2
 
 # Create your views here.
 
 def home(request):
-    return HttpResponse('<h1> ASESORAMIENTOS </h1>')
+    return HttpResponse('<h1> ASESORAMIENTOS </h1>' + rpy2.__version__)
