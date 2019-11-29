4a329a28c791b077a13b12f18f7e4c5d77bd5a01
Caronte Hades <caronte.hades@gmail.com>
2019-11-29 12:40:40 +0100

    support sessions con admin

--- udaic_support_sessions/admin.py
+++ udaic_support_sessions/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+from .models import SupportPerson, SupportSession, BaseSupport
+
 # Register your models here.
+admin.site.register(SupportPerson)
+admin.site.register(SupportSession)
+admin.site.register(BaseSupport)
--- udaic_support_sessions/models.py
+++ udaic_support_sessions/models.py
@@ -2,12 +2,14 @@ from django.db import models
 from django.conf import settings
 from django.contrib.auth import get_user_model
 from django.template.defaultfilters import slugify
+from django.urls import reverse
 
 __author__ = 'Fernando Andres Pretel'
 
 # Create your models here.
 
 #TODO: check for a better solution
+
 def get_sentinel_user():
     return get_user_model().objects.get_or_create(username='deleted')[0]
 
@@ -19,8 +21,8 @@ class SupportPerson(models.Model):
     last_name = models.CharField(null=True, max_length=200)
 
     class Meta:
-        verbose_name = "usuario UdAIC"
-        verbose_name_plural = "usuarios UdAIC"
+        verbose_name = "Usuario UdAIC"
+        verbose_name_plural = "Usuarios UdAIC"
 
     def __str__(self):
         texto = "Usuario UdAIC: %s" % self.name % self.last_name
@@ -29,15 +31,14 @@ class SupportPerson(models.Model):
 
 class BaseSupport(models.Model):
     id = models.AutoField(primary_key=True)
-    client_user = models.ManyToManyField(SupportPerson.id, blank=True, null=True)
-    support_user = models.ManyToManyField(SupportPerson.id, blank=True, null=True, on_delete=models.)
+    client_user = models.ManyToManyField(SupportPerson, blank=True, null=True)
+    support_user = models.ManyToManyField(SupportPerson, blank=True, null=True)
     title = models.CharField(null=True, max_length=200)
     creation_date = models.DateField(auto_now_add=True)
     slug = models.SlugField(unique=True, max_length=255)
 
-    @models.permalink
     def get_absolute_url(self):
-        return ('asesoramiento_detalle', (),
+        return reverse('asesoramiento_detalle', (),
                 {
                     'slug': self.slug,
                 })
@@ -48,8 +49,8 @@ class BaseSupport(models.Model):
         super(BaseSupport, self).save(*args, **kwargs)
 
     class Meta:
-        verbose_name = "asesoramiento base"
-        verbose_name_plural = "udaic_support_sessions base"
+        verbose_name = "Asesoramiento Base"
+        verbose_name_plural = "Asesoramientos Base"
 
     def __str__(self):
         texto = "Asesoramiento base: %s" % self.title
